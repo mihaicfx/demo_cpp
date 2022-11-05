498ca800a8306a19e459a8c16dd0156be76fe3a3
Mihai Feier <mihaifx@gmail.com>
2022-11-05 13:53:37 +0200

    Added unit-tests

---
+++ tests/unit/dictionary_test.cpp
@@ -0,0 +1,61 @@
+#include <gtest/gtest.h>
+
+#include "dictionary.h"
+
+
+TEST(Dictionary, get_onMissingValue_fails)
+{
+    Dictionary dict{false};
+
+    EXPECT_FALSE(dict.get("abc"));
+}
+
+TEST(Dictionary, get_onExistingValue_succeds)
+{
+    Dictionary dict{false};
+
+    dict.set("abc", "123");
+
+    auto valFromDict = dict.get("abc");
+    EXPECT_EQ(valFromDict, "123");
+}
+
+TEST(Dictionary, set_multipleValues_succeds)
+{
+    Dictionary dict{false};
+
+    dict.set("abc", "123");
+    dict.set("def", "456");
+
+    EXPECT_TRUE(dict.get("abc"));
+    EXPECT_TRUE(dict.get("def"));
+    EXPECT_FALSE(dict.get("ghi"));
+}
+
+TEST(Dictionary, get_withBloomFilter_succeeds)
+{
+    Dictionary dict{true};
+
+    dict.set("abc", "123");
+    dict.set("def", "456");
+
+    EXPECT_TRUE(dict.get("abc"));
+    EXPECT_TRUE(dict.get("def"));
+    EXPECT_FALSE(dict.get("ghi"));
+}
+
+TEST(Dictionary, stats_returnsExpectedValues)
+{
+    Dictionary dict{true};
+
+    dict.set("abc", "123");
+    dict.set("def", "456");
+    dict.get("abc");
+    dict.get("ghi");
+
+    const auto stats = dict.stats();
+    EXPECT_EQ(stats.nTotalGet, 2);
+    EXPECT_EQ(stats.nTotalSet, 2);
+    EXPECT_EQ(stats.nSuccessfulGet, 1);
+    EXPECT_EQ(stats.nFailedGet, 1);
+}
